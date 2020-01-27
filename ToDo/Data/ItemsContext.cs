d79de1f5be26ff655379aefb6221e49cbc9c9a4e
kokaibalint <Blint@DESKTOP-5PUQ61G>
2020-01-27 14:32:03 +0100

    Model added

---
+++ ToDo/Data/ItemsContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.Data
+{
+    public class ItemsContext : DbContext
+    {
+        public ItemsContext(DbContextOptions<ItemsContext> options) : base(options) { }
+        public DbSet<Models.Item> Items { get; set; }
+    }
+}
